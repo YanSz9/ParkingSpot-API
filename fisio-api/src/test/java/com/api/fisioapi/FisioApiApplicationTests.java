773f305fd74dce6b242d31e30b2c52d05a6e64a2
Yan Souza <yansouza009@gmail.com>
2023-03-24 20:15:25 -0300

    Subindo Api

---
+++ fisio-api/src/test/java/com/api/fisioapi/FisioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fisioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
